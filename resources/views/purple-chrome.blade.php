d639d2d0a6f387188a361a64858d890f1a4ee363
Nick Evans <nick.evans@northwestern.edu>
2023-03-29 10:21:56 -0500

    Bootstrap 5 + Vite support

--- resources/views/purple-chrome.blade.php
+++ resources/views/purple-chrome.blade.php
@@ -6,7 +6,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ isset($page_title) ? $page_title : config('app.name') }} - Northwestern University</title>
-    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    @vite(['resources/sass/app.scss'])
     <link href="https://common.northwestern.edu/v8/icons/favicon-16.png" rel="icon" sizes="16x16" type="image/png">
     <link href="https://common.northwestern.edu/v8/icons/favicon-32.png" rel="icon" sizes="32x32" type="image/png">
 
@@ -15,37 +15,42 @@
     @endif
   </head>
   <body class="{{ isset($bodyClasses) ? $bodyClasses : 'd-flex flex-column min-vh-100' }}" {!! isset($bodyAttributes) ? $bodyAttributes : '' !!}>
+
     <div class="flex-grow-1" id="app">
         <nav class="navbar navbar-inverse bg-dark-purple" aria-label="Logo">
-            <a class="navbar-brand" href="/">
-                <img
-                    src="{{ isset($navbar_custom_lockup_url) ? $navbar_custom_lockup_url : config('northwestern-theme.lockup') }}"
-                    alt='{{ isset($navbar_custom_lockup_alt) ? $navbar_custom_lockup_alt : config('app.name') }}'
-                >
-            </a>
+            <div class="container-fluid">
+                <a class="navbar-brand" href="/">
+                    <img
+                        src="{{ isset($navbar_custom_lockup_url) ? $navbar_custom_lockup_url : config('northwestern-theme.lockup') }}"
+                        alt='{{ isset($navbar_custom_lockup_alt) ? $navbar_custom_lockup_alt : config('app.name') }}'
+                    >
+                </a>
 
-            @if (config('app.env') !== 'production')
-            <span class="badge badge-warning d-none d-sm-inline" style="font-size: large;">
-                <i class="fa fa-exclamation-triangle" aria-hidden="true"></i> Environment: {{ config('app.env') }}
-                @includeIf('db-schema-display')
-            </span>
+                @if (config('app.env') !== 'production')
+                <span class="badge bg-warning text-black d-none d-sm-inline" style="font-size: large;">
+                    <i class="fa fa-exclamation-triangle" aria-hidden="true"></i> Environment: {{ config('app.env') }}
+                    @includeIf('db-schema-display')
+                </span>
 
-            {{-- Need to format a bit differently for small screens, or it looks un-designed --}}
-            <span class="badge badge-warning d-inline d-sm-none mx-auto my-2" style="font-size: large;">
-                <i class="fa fa-exclamation-triangle" aria-hidden="true"></i> Environment: {{ config('app.env') }}
-                @includeIf('db-schema-display')
-            </span>
-            @endif
-        </nav>
-
-        <nav class="navbar navbar-expand-md navbar-dark bg-primary"  aria-label="Navbar">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarCollapse">
-                @include('layout._nav')
+                {{-- Need to format a bit differently for small screens, or it looks un-designed --}}
+                <span class="badge bg-warning text-black d-inline d-sm-none mx-auto my-2" style="font-size: large;">
+                    <i class="fa fa-exclamation-triangle" aria-hidden="true"></i> Environment: {{ config('app.env') }}
+                    @includeIf('db-schema-display')
+                </span>
+                @endif
             </div>
         </nav>
+        <nav class="navbar navbar-expand-md navbar-dark bg-primary" aria-label="Navbar">
+            <div class="container-fluid">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    @include('layout._nav')
+                </div>
+            </div>
+        </nav>
+
         <main role="main">
             @yield('container')
         </main>
@@ -64,10 +69,11 @@
                 <li><a href='https://www.northwestern.edu/disclaimer.html'>Disclaimer</a></li>
                 <li><a href='https://www.northwestern.edu/emergency/index.html'>Campus Emergency Information</a></li>
                 <li><a href='https://policies.northwestern.edu/'>University Police</a></li>
+                <li><a href='https://www.northwestern.edu/accessibility/about/report-accessibility-issue.html'>Report an Accessibility Issue</a></li>
             </ul>
         </div>
         <div class="col-md-4 col-lg-3">
-            <dl class="row">
+            <dl class="d-flex justify-content-start">
                 <dt class="address-pin"><span class="sr-only">Office Address</span></dt>
                 <dd>
                     <address>
@@ -80,12 +86,12 @@
                 </dd>
             </dl>
 
-            <dl class="row pb-2">
+            <dl class="d-flex justify-content-start pb-2">
                 <dt class="phone-pin"><span class="sr-only">Phone Number</span></dt>
                 <dd class="mb-0">{{ config('northwestern-theme.office.phone') }}</dd>
             </dl>
 
-            <dl class="row">
+            <dl class="d-flex justify-content-start pb-2">
                 <dt class="email-pin"><span class="sr-only">Email Address</span></dt>
                 <dd class="mb-0"><a href='mailto:{{ config('northwestern-theme.office.email') }}'>{{ config('northwestern-theme.office.email') }}</a></dd>
             </dl>
@@ -104,7 +110,7 @@
         @livewireScripts()
     @endif
 
-    <script src="{{ mix('js/app.js') }}"></script>
+    @vite(['resources/js/app.js'])
 
     @isset($sentry_config['dsn'])
     <script type="text/javascript">
