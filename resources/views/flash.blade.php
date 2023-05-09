468ba42c2a6027edcb2c498f52a581ae1e379754
Danny Foster <danny@foster.sh>
2023-05-09 16:12:28 -0500

    Bootstrap 5 close button fix (#127)
    * fix(flash): BS5 close button
    
    * fix: namespaced data dismiss attribute

--- resources/views/flash.blade.php
+++ resources/views/flash.blade.php
@@ -1,8 +1,6 @@
 @if (Session::has('status'))
 <div class="alert alert-info alert-dismissible fade show w-100" role="alert">
-  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-    <span aria-hidden="true">&times;</span>
-  </button>
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
   {{ Session::get('status') }}
 </div>
 @endif
