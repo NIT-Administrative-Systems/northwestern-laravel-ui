3d64b1d3f9bc29cacdad7133c2f20f4a02c14b14
Danny Foster <danny@northwestern.edu>
2023-05-09 16:05:52 -0500

    fix: namespaced data dismiss attribute

--- resources/views/flash.blade.php
+++ resources/views/flash.blade.php
@@ -1,6 +1,6 @@
 @if (Session::has('status'))
 <div class="alert alert-info alert-dismissible fade show w-100" role="alert">
-  <button type="button" class="btn-close" data-dismiss="alert" aria-label="Close"></button>
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
   {{ Session::get('status') }}
 </div>
 @endif
