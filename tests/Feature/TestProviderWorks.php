60ae253af79ba750d9c092e0de9f0984c55730ca
Nick Evans <nick.evans@northwestern.edu>
2024-03-19 10:19:12 -0500

    PHPUnit 10 Shift (#174)

--- tests/Feature/TestProviderWorks.php
+++ tests/Feature/TestProviderWorks.php
@@ -1,22 +1,24 @@
 <?php
 
-namespace Northwestern\SysDev\UI\Tests;
+namespace Northwestern\SysDev\UI\Tests\Feature;
 
 use Illuminate\Support\Str;
 use Northwestern\SysDev\UI\Providers\NorthwesternUiServiceProvider;
+use Northwestern\SysDev\UI\Tests\TestCase;
+use PHPUnit\Framework\Attributes\Test;
 
-class TestProviderWorks extends TestCase
+final class TestProviderWorks extends TestCase
 {
-    /** @test */
-    public function config_loads()
+    #[Test]
+    public function config_loads(): void
     {
         // Makes sure the provider is working
         // It's not much, but we can't really test a huge heap of views?
         $this->assertNotNull(config('northwestern-theme.lockup'));
     }
 
-    /** @test */
-    public function renders()
+    #[Test]
+    public function renders(): void
     {
         $this->app['router']->get(__METHOD__, function () {
             return view('northwestern::demo-styles');
@@ -28,8 +30,8 @@ class TestProviderWorks extends TestCase
             ->assertDontSee('Sentry.init');
     }
 
-    /** @test */
-    public function sentry_configured()
+    #[Test]
+    public function sentry_configured(): void
     {
         $this->app['config']->set('northwestern-theme.sentry-dsn', 'sentry-dsn-mocked-value-woohoo');
 
@@ -44,8 +46,8 @@ class TestProviderWorks extends TestCase
             ->assertSee(config('northwestern-theme.sentry-dsn'));
     }
 
-    /** @test */
-    public function sentry_user_context()
+    #[Test]
+    public function sentry_user_context(): void
     {
         $this->app['config']->set('northwestern-theme.sentry-dsn', 'sentry-dsn-mocked-value-woohoo');
 
