baa6f3c473f8558ebd96f1657e456e93ba7bb906
Nick Evans <nick.evans@northwestern.edu>
2024-03-19 10:51:31 -0500

    Migrate to VuePress v2

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,21 +1,50 @@
-module.exports = {
-    base: '/northwestern-laravel-ui/',
+import { defineUserConfig } from 'vuepress';
+import defaultTheme from '@vuepress/theme-default';
+import { viteBundler } from '@vuepress/bundler-vite'
+import { searchProPlugin } from 'vuepress-plugin-search-pro';
+import { mdEnhancePlugin } from 'vuepress-plugin-md-enhance';
+
+export default defineUserConfig({
     title: 'Northwestern Bootstrap Preset for Laravel',
     description: 'Adds a Northwestern Bootstrap theme into your Laravel application.',
-    dest: '.build',
+    head: [
+        ['link', { href: 'https://common.northwestern.edu/v8/icons/favicon-16.png', rel: 'icon', sizes: '16x16', type: 'image/png' }],
+        ['link', { href: 'https://common.northwestern.edu/v8/icons/favicon-32.png', rel: 'icon', sizes: '32x32', type: 'image/png' }],
+        ['link', { href: 'https://use.fontawesome.com/releases/v6.4.0/css/all.css', rel: 'stylesheet'}],
+    ],
+    pagePatterns: ['**/*.md', '!**/README.md', '!.vuepress', '!node_modules'],
+    base: '/northwestern-laravel-ui/',
 
-    themeConfig: {
-        displayAllHeaders: true,
+    bundler: viteBundler({
+        viteOptions: {},
+        vuePluginOptions: {},
+    }),
+
+    theme: defaultTheme({
         repo: 'NIT-Administrative-Systems/northwestern-laravel-ui',
-        docsDir: 'docs',
         docsBranch: 'develop',
-        editLinks: true,
+        docsDir: 'docs',
+        editLink: true,
         editLinkText: 'Edit Page',
         lastUpdated: true,
+        sidebarDepth: 1,
 
         sidebar: [
-            ['/', 'Getting Started'],
-            'usage',
+            { text: 'Getting Started', link: '/' },
+            '/usage'
         ],
-    },
-}
+    }),
+    plugins: [
+        searchProPlugin({
+            indexContent: true,
+            searchDelay: 500,
+            autoSuggestions: false,
+        }),
+        mdEnhancePlugin({
+            tabs: true,
+            footnote: true,
+            mark: true,
+            include: true,
+        })
+    ],
+});
