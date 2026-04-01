5b3f5173833c0145db35a2fb850bfcfe057c7b98
Nick Evans <nick.evans@northwestern.edu>
2026-04-01 09:27:49 -0500

    Fix base path

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -5,7 +5,7 @@ import northwesternTheme from "@nu-appdev/northwestern-starlight-theme";
 // https://astro.build/config
 export default defineConfig({
   site: "https://nit-administrative-systems.github.io/northwestern-laravel-ui/",
-  base: "/SysDev-laravel-soa/",
+  base: "/northwestern-laravel-ui/",
   integrations: [
     starlight({
       plugins: [northwesternTheme()],
